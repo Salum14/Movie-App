fb09134aa95363626c70e38352ff4220c7f5fd81
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-3.local>
2026-03-19 13:59:22 -0400

    setup environment variables

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,16 +1,19 @@
-const API_KEY = "361c8a6da869a3d51c3a661a5a93766f"
-const BASE_URL = "https://api.themoviedb.org/3"
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+console.log("API KEY:", API_KEY);
+
 
 export const getPopularMovies = async () => {
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
-    const data = await response.json()
-    return data.results 
-}
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
 
 export const searchMovies = async (query) => {
-    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
-    const data = await response.json()
-    return data.results 
-}
-
-
+    const response = await fetch(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
+    );
+    const data = await response.json();
+    return data.results;
+};
